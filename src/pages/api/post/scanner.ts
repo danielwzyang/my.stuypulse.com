2a2e6f9b217d9445ceff302ca2b29a5bce4062dd
Daniel Yang <danielwzyang@gmail.com>
2024-12-17 20:22:22 -0500

    added meetings upload to api

--- src/pages/api/post/scanner.ts
+++ src/pages/api/post/scanner.ts
@@ -4,73 +4,104 @@ import type { APIRoute } from "astro"
 import { serviceSupabase } from "../../../lib/supabase"
 
 export const POST: APIRoute = async ({ request }) => {
-    // attemping to fix csrf error with CORS headers
-    const headers = { "Access-Control-Allow-Origin": "*" }
-
     try {
         const formData = await request.formData()
 
         // make sure the key is correct
         const key = request.headers.get("key")
-        if (!key || key != import.meta.env.KIOSK_KEY) return new Response("Authorization key is incorrect.", { status: 403, headers })
+        if (!key || key != import.meta.env.KIOSK_KEY) return new Response("Authorization key is incorrect.", { status: 403 })
 
         // fetch data from supabase
-        let { data: meetings, error } = await serviceSupabase
+        let { data: attendance, error } = await serviceSupabase
             .from("attendance")
             .select("id_number, checked_out, only_checked_in")
 
-        if (error || !meetings) return new Response("Error fetching user meetings: " + error?.message, { status: 500, headers })
+        if (error || !attendance) return new Response("Error fetching user attendance: " + error?.message, { status: 500 })
+
+        let { data: meetings, error: error1 } = await serviceSupabase
+            .from("meetings")
+            .select("*")
+        
+        if (error1 || !meetings) return new Response("Error fetching meetings: " + error1?.message, { status: 500 })
 
         // converts the array of objects into one object
-        const meetingsObj = meetings.reduce((obj, { id_number, checked_out, only_checked_in }) => {
+        const attendanceObj = attendance.reduce((obj, { id_number, checked_out, only_checked_in }) => {
             obj[id_number] = { checked_out: new Set(checked_out || []), only_checked_in: new Set(only_checked_in || []) }
             return obj
         }, {} as Record<string, { checked_out: Set<string>, only_checked_in: Set<string> }>)
 
+        const meetingsObj = meetings.reduce((obj, { date, attendees, num_checkins, num_checkouts, checkout_rate_percent }) => {
+            obj[date] = { attendees: new Set(attendees || []), num_checkins, num_checkouts, checkout_rate_percent }
+            return obj
+        }, {} as Record<string, { attendees: Set<string>, num_checkins: number, num_checkouts: number, checkout_rate_percent: string }>)
+
+        // read the meetings file
+        const meetingsFile = formData.get("meetings") as File
+        if (!meetingsFile) return new Response("Meetings file missing.", { status: 500 })
+
+        // parse the meetings data
+        const meetingsText = await new Blob([meetingsFile], { type: "text/csv" }).text()
+        let meetingsData = meetingsText.trim().split("\n").map((e) => e.split(","))
+        meetingsData.shift() // gets rid of the headers row
+
+        // updates the meetings
+        meetingsData.forEach((row) => {
+            const [date, num_checkins, num_checkouts, checkout_rate_percent] = row
+            // adds the key value pair if the key doesn't exist (ie the date is new)
+            if (!(date in meetingsObj))
+                meetingsObj[new Date(date).toISOString().split("T")[0]] = { attendees: new Set(), num_checkins, num_checkouts, checkout_rate_percent }
+
+            // adds the meeting to the list of all the meetings
+            attendanceObj["all"].checked_out.add(date)
+        })
+
         // read the checkin file
         const checkinsFile = formData.get("checkins") as File
-        const checkinsText = await new Blob([checkinsFile], { type: "text/csv" }).text()
+        if (!checkinsFile) return new Response("Checkins file missing.", { status: 500 })
 
         // parse the checkin data
+        const checkinsText = await new Blob([checkinsFile], { type: "text/csv" }).text()
         let checkinsData = checkinsText.trim().split("\n").map((e) => e.split(","))
         checkinsData.shift() // gets rid of the headers row
 
-        // updates the user meetings
+        // updates the user attendance
         checkinsData.forEach((row) => {
-            const date = row[0], id = row[1], userCheckedOut = row[5] !== ""
+            const [date, id, , , checkin_time, checkout_time, total_hours] = row
 
             // inits the key value pair if the key doesn't exist (ie the id is new)
-            if (!(id in meetingsObj))
-                meetingsObj[id] = { checked_out: new Set(), only_checked_in: new Set() }
+            if (!(id in attendanceObj))
+                attendanceObj[id] = { checked_out: new Set(), only_checked_in: new Set() }
 
             // adds the new meeting to the correct list
-            meetingsObj[id][userCheckedOut ? "checked_out" : "only_checked_in"].add(date)
+            attendanceObj[id][checkout_time !== "" ? "checked_out" : "only_checked_in"].add(date)
 
             // removes the date from the only checked in if they have now checked out
-            if (userCheckedOut) meetingsObj[id]["only_checked_in"].delete(date)
+            if (checkout_time !== "") attendanceObj[id]["only_checked_in"].delete(date)
 
-            // adds the meeting to the list of all the meetings
-            meetingsObj["all"].checked_out.add(date)
+            // adds student to attendees of meeting
+            if (!(id in meetingsObj[date].attendees))
+                meetingsObj[date].attendees.add({ id, checkin_time, checkout_time, total_hours })
         })
 
         // read the attendance file
         const attendanceFile = formData.get("attendance") as File
-        const attendanceText = await new Blob([attendanceFile], { type: "text/csv" }).text()
+        if (!attendanceFile) return new Response("Attendance file missing.", { status: 500 })
 
         // parse the attendance data
+        const attendanceText = await new Blob([attendanceFile], { type: "text/csv" }).text()
         let attendanceData = attendanceText.trim().split("\n").map((e) => e.split(","))
         attendanceData.shift() // gets rid of the headers row
 
-        // convert the data into an array of objects
-        const newData = attendanceData.map((
+        // convert the attendance data into an array of objects
+        const newAttendanceData = attendanceData.map((
             [id_number, first_name, last_name, num_checkins, attendance_rate_percent, num_checkouts, checkout_rate_percent, total_hours, average_hours]) => {
-            const checked_out = Array.from(meetingsObj[id_number].checked_out)
-            const only_checked_in = Array.from(meetingsObj[id_number].only_checked_in)
+            const checked_out = Array.from(attendanceObj[id_number].checked_out)
+            const only_checked_in = Array.from(attendanceObj[id_number].only_checked_in)
 
             return { id_number, first_name, last_name, num_checkins, attendance_rate_percent, num_checkouts, checkout_rate_percent, total_hours, average_hours, checked_out, only_checked_in }
         })
         
-        newData.push( { 
+        newAttendanceData.push( { 
             id_number: "all", 
             first_name: "",
             last_name: "",
@@ -80,19 +111,33 @@ export const POST: APIRoute = async ({ request }) => {
             checkout_rate_percent: "", 
             total_hours: "0",
             average_hours: "0",
-            checked_out: Array.from(meetingsObj["all"].checked_out),
-            only_checked_in: Array.from(meetingsObj["all"].only_checked_in) 
+            checked_out: Array.from(attendanceObj["all"].checked_out),
+            only_checked_in: Array.from(attendanceObj["all"].only_checked_in) 
         })
 
         // upsert the attendance data
         const { error: error2 } = await serviceSupabase
             .from("attendance")
-            .upsert(newData)
+            .upsert(newAttendanceData)
 
-        if (error2) return new Response("Error upserting attendance data: " + error2.message, { status: 500, headers })
+        if (error2) return new Response("Error upserting attendance data: " + error2.message, { status: 500 })
 
-        return new Response("Successfully posted data.", { status: 200, headers })
+        // convert the meetings data into an array of objects
+        const newMeetingsData = []
+        for (const date in meetingsObj) {
+            const { attendees, num_checkins, num_checkouts, checkout_rate_percent } = meetingsObj[date]
+            newMeetingsData.push({ date, attendees: Array.from(attendees), num_checkins, num_checkouts, checkout_rate_percent })
+        }
+
+        // upsert the meetings data
+        const { error: error3 } = await serviceSupabase
+            .from("meetings")
+            .upsert(newMeetingsData)
+
+        if (error3) return new Response("Error upserting meetings data: " + error3.message, { status: 500 })
+
+        return new Response("Successfully posted data.", { status: 200 })
     } catch (error) {
-        return new Response("Error posting data: " + error, { status: 500, headers })
+        return new Response("Error posting data: " + error, { status: 500 })
     }
 }
